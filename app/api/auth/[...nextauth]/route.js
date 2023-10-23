5dfc53b322645422de6e0606642710b67490e321
Lakhan Singh Barod <lakhanbarod165@gmail.com>
2023-10-23 08:53:32 +0530

    fix username validation typo

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -37,7 +37,7 @@ const handler = NextAuth({
 
         return true
       } catch (error) {
-        console.log("Error checking if user exists: ", error.message);
+        console.log("Error checking if user exists: ", error);
         return false
       }
     },
